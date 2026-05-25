6d46baf29a14c363785711a0637a61c71bec2e02
Sukant Ravichandran <sukantravi005@gmail.com>
2026-05-25 21:09:08 +0530

    Time: 0 ms (100%), Space: 44.3 MB (86.3%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int[] val:accounts){
+                int sums=0;
+            for(int val1:val){
+                sums+=val1;
+            }
+            if(max<sums) max=sums;
+        }
+        // System.out.println(accounts.length);
+        return max;
+    }
+}
